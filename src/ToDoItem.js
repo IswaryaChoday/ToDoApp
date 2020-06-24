4203e623f07460ad16718411de94192adb1228e3
Ishwarya Bolla <ishwarya.bolla96@gmail.com>
2020-06-24 13:29:29 -0400

    Added handleChange() and setState() functions

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -3,9 +3,13 @@ import React from "react"
 function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed}/>
-            <p>{props.item.text}</p>
-        </div>
+        <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => props.handleChange(props.item.id)}
+            />
+        <p>{props.item.text}</p>
+    </div>
     )
 }
 
